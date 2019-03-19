208a323582d42756273663474a5e1041a21e095f
Pawel Kreciwilk <pawel.kreciwilk@gmail.com>
2019-03-19 11:24:27 +0100

    File watcher

--- posts/index.js
+++ posts/index.js
@@ -1,15 +1,32 @@
 const express = require("express");
-const posts = require("./fakes/posts")
+const chokidar = require('chokidar');
+const fs = require("fs");
+
+chokidar
+    .watch('./fakes/posts.json')
+    .on('all', (event, path) => {
+        try
+        {
+            console.log(event);
+            const fileContent = fs.readFileSync(path).toString();
+            this.posts = JSON.parse(fileContent);
+            console.log(this.posts);
+        }
+        catch(err)
+        {
+            console.log(err);
+        }
+    });
 
 const app = express();
 
 app.get('/',(req, res) => {
-    res.json(posts);
+    res.json(this.posts);
 });
 app.get('/:id',(req, res) => {
     const postId = req.params.id;
 
-    res.json(posts.find(p => p.id === postId));
+    res.json(this.posts.find(p => p.id === postId));
 });
 app.get('*',(req, res) => res.status(404));
 
